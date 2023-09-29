e61a2b1d69d42a2e6cdbe178be1d0315d8ea8e5a
Tomas Votruba <tomas.vot@gmail.com>
2023-09-29 14:24:25 +0200

    misc

--- config/sets/phpspec-to-phpunit.php
+++ config/sets/phpspec-to-phpunit.php
@@ -16,6 +16,8 @@ use Rector\PhpSpecToPHPUnit\Rector\Variable\MockVariableToPropertyFetchRector;
 // see: https://gnugat.github.io/2015/09/23/phpunit-with-phpspec.html
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rules([
+        \Rector\PhpSpecToPHPUnit\Rector\Namespace_\RenameSpecNamespacePrefixToTestRector::class,
+
         // 1. first convert mocks
         PhpSpecMocksToPHPUnitMocksRector::class,
         PhpSpecPromisesToPHPUnitAssertRector::class,
