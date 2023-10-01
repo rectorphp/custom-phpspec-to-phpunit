14800e1bf22aa08321e8b8cdd875fc40646683f7
Tomas Votruba <tomas.vot@gmail.com>
2023-10-01 11:28:19 +0200

    fixture

--- src/NodeAnalyzer/PhpSpecBehaviorNodeDetector.php
+++ src/NodeAnalyzer/PhpSpecBehaviorNodeDetector.php
@@ -4,29 +4,15 @@ declare(strict_types=1);
 
 namespace Rector\PhpSpecToPHPUnit\NodeAnalyzer;
 
-use PhpParser\Node\Expr\MethodCall;
-use PhpParser\Node\Stmt\Class_;
-use PhpParser\Node\Stmt\ClassLike;
-use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\ClassReflection;
-use PHPStan\Type\ObjectType;
 use Rector\NodeTypeResolver\Node\AttributeKey;
-use Rector\NodeTypeResolver\NodeTypeResolver;
 
 final class PhpSpecBehaviorNodeDetector
 {
-    public function __construct(
-        private readonly NodeTypeResolver $nodeTypeResolver,
-    ) {
-    }
-
-    public function isInPhpSpecBehavior(Class_|ClassMethod|MethodCall $node): bool
+    public function isInPhpSpecBehavior(Node $node): bool
     {
-        if ($node instanceof ClassLike) {
-            return $this->nodeTypeResolver->isObjectType($node, new ObjectType('PhpSpec\ObjectBehavior'));
-        }
-
         $scope = $node->getAttribute(AttributeKey::SCOPE);
         if (! $scope instanceof Scope) {
             return false;
