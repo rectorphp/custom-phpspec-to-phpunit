2334f3ed0b3bb75e8ed9b213db83185a39844a41
Tomas Votruba <tomas.vot@gmail.com>
2023-10-01 16:59:48 +0200

    separate test

---
+++ tests/Sets/Fixture/get_wrapped_object.php
@@ -0,0 +1,285 @@
+<?php
+
+namespace Sets\Fixture;
+
+use Tests\Rector\PhpSpecToPHPUnit\Tests\Rector\Variable\PhpSpecToPHPUnitRector\Fixture\SomeType;
+
+class BlablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest extends \PHPUnit\Framework\TestCase
+{
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTestTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTestTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blablaTest;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blablaTest = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blabla;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blabla = new \Sets\Fixture\Blabla();
+    }
+    private \Sets\Fixture\Blabla $blabla;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->blabla = new \Sets\Fixture\Blabla();
+    }
+    public function testMe(SomeType $someType)
+    {
+        $assignMe = $someType;
+    }
+}
+
+?>
--- tests/Sets/FullSetTest.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-
-namespace Sets;
-
-class FullSetTest
-{
-
-}
