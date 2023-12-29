5a68770b57091354d25e4ad0093bbf0de40f52ec
GitHub Action <action@github.com>
2023-12-29 19:24:46 +0000

    release PHP 7.2 downgraded

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rector/custom-phpspec-to-phpunit',
+        'pretty_version' => '0.3.8',
+        'version' => '0.3.8.0',
+        'reference' => 'fe01d6b43cd8d5e23fae8d0d8920673e5034ae05',
+        'type' => 'rector-extension',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'rector/custom-phpspec-to-phpunit' => array(
+            'pretty_version' => '0.3.8',
+            'version' => '0.3.8.0',
+            'reference' => 'fe01d6b43cd8d5e23fae8d0d8920673e5034ae05',
+            'type' => 'rector-extension',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
