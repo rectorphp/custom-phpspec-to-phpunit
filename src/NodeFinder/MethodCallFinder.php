82e4ad64ff13ab8e1e569c6d12ce8818db7af11d
Tomas Votruba <tomas.vot@gmail.com>
2023-12-31 00:38:17 +0000

    Fix never() and once()

--- src/NodeFinder/MethodCallFinder.php
+++ src/NodeFinder/MethodCallFinder.php
@@ -7,22 +7,21 @@ namespace Rector\PhpSpecToPHPUnit\NodeFinder;
 use PhpParser\Node;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Identifier;
-use PhpParser\Node\Stmt;
 use PhpParser\NodeFinder;
 
 final class MethodCallFinder
 {
-    public static function hasByName(Stmt $stmt, string $desiredMethodName): bool
+    public static function hasByName(\PhpParser\Node $node, string $desiredMethodName): bool
     {
-        $foundMethodCall = self::findByName($stmt, $desiredMethodName);
+        $foundMethodCall = self::findByName($node, $desiredMethodName);
         return $foundMethodCall instanceof MethodCall;
     }
 
-    public static function findByName(Stmt $stmt, string $desiredMethodName): ?MethodCall
+    public static function findByName(\PhpParser\Node $node, string $desiredMethodName): ?MethodCall
     {
         $nodeFinder = new NodeFinder();
 
-        $foundMethodCall = $nodeFinder->findFirst($stmt, static function (Node $node) use ($desiredMethodName): bool {
+        $foundMethodCall = $nodeFinder->findFirst($node, static function (Node $node) use ($desiredMethodName): bool {
             if (! $node instanceof MethodCall) {
                 return false;
             }
