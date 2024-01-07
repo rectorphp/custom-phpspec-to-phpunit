0edd8b873d2fdb03ff67e61e94f4c815a1f4268d
GitHub Action <action@github.com>
2024-01-07 14:15:19 +0000

    release PHP 7.2 downgraded

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rector/custom-phpspec-to-phpunit',
+        'pretty_version' => '0.4.3',
+        'version' => '0.4.3.0',
+        'reference' => '9a0bca0844b607518e4dd800a4bc7ef1f6faf28e',
+        'type' => 'rector-extension',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'rector/custom-phpspec-to-phpunit' => array(
+            'pretty_version' => '0.4.3',
+            'version' => '0.4.3.0',
+            'reference' => '9a0bca0844b607518e4dd800a4bc7ef1f6faf28e',
+            'type' => 'rector-extension',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'symfony/finder' => array(
+            'pretty_version' => 'v7.0.0',
+            'version' => '7.0.0.0',
+            'reference' => '6e5688d69f7cfc4ed4a511e96007e06c2d34ce56',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/finder',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
