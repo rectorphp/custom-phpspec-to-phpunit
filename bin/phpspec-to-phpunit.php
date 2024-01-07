5d4e1269073a6dd98d9a4a62b5092bdfada0d4b8
Tomas Votruba <tomas.vot@gmail.com>
2024-01-07 15:22:21 +0100

    add smart scoped autoload

--- bin/phpspec-to-phpunit.php
+++ bin/phpspec-to-phpunit.php
@@ -5,7 +5,22 @@ declare(strict_types=1);
 use Rector\PhpSpecToPHPUnit\Command\RenameSuffixCommand;
 use Symfony\Component\Console\Application;
 
-require __DIR__ . '/../vendor/autoload.php';
+$possibleAutoloadPaths = [
+    // dependency
+    __DIR__ . '/../../../autoload.php',
+    // monorepo
+    __DIR__ . '/../../../vendor/autoload.php',
+    // after split package
+    __DIR__ . '/../vendor/autoload.php',
+];
+
+foreach ($possibleAutoloadPaths as $possibleAutoloadPath) {
+    if (file_exists($possibleAutoloadPath)) {
+        require_once $possibleAutoloadPath;
+        break;
+    }
+}
+
 
 $application = new Application();
 $application->add(new RenameSuffixCommand());
